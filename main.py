5a2fc26d3d42cef4ad06be97678fb392182e4284
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-11 09:11:35 -0400

    polygons commit

--- main.py
+++ main.py
@@ -12,10 +12,23 @@ import random
 if __name__ == "__main__":
     SIZE = 400;
     t = TransformationMatrix.identity().rotate_y_about_point(
-        45, 200, 200, 0).rotate_z_about_point(0, 200, 200, 0)
-    a = Generator.get_torus_polygonmatrix(200, 200, 0, 80, 120)
-    b = Generator.get_box_polygonmatrix(200, 200, 0, 50, 80, 120)
-    d = Drawing(SIZE, SIZE)
+        45, 200, 200, 0).rotate_z_about_point(0, 200, 200, 0).translate(
+        -20, 30, 0)
+    drawing = Drawing(SIZE, SIZE)
     view = Vector([0, 0, 1])
-    d.draw_polygonmatrix((a * t).cull_backfaces(view), Color("FF0000"))
-    d.display()
+
+    a = Generator.get_torus_polygonmatrix(350, 50, 0, 20, 40, 20, 20)
+    b = Generator.get_torus_polygonmatrix(250, 100, 0, 15, 30, 20, 20)
+    c = Generator.get_torus_polygonmatrix(175, 140, 0, 10, 20, 20, 20)
+    d = Generator.get_torus_polygonmatrix(175, 200, 0, 10, 20, 20, 20)
+    e = Generator.get_torus_polygonmatrix(250, 240, 0, 15, 30, 20, 20)
+    f = Generator.get_torus_polygonmatrix(350, 290, 0, 20, 40, 20, 20)
+
+    drawing.draw_polygonmatrix((a * t).cull_backfaces(view), Color("FF0000"))
+    drawing.draw_polygonmatrix((b * t).cull_backfaces(view), Color("00FF00"))
+    drawing.draw_polygonmatrix((c * t).cull_backfaces(view), Color("0000FF"))
+    drawing.draw_polygonmatrix((d * t).cull_backfaces(view), Color("FF0000"))
+    drawing.draw_polygonmatrix((e * t).cull_backfaces(view), Color("00FF00"))
+    drawing.draw_polygonmatrix((f * t).cull_backfaces(view), Color("0000FF"))
+    drawing.display()
+    drawing.generate("test", "png")
