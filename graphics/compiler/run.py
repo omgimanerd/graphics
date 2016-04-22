d3af554df48320e9c3b08ff3ce6cf2cf972f42c3
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-22 14:35:05 -0400

    more formatting

--- graphics/compiler/run.py
+++ graphics/compiler/run.py
@@ -1,22 +1,51 @@
+#!/usr/bin/python
+# This takes an mdl file, compiles it, and runs the commands contained in it.
+# Author: alvin.lin.dev@gmail.com (Alvin Lin)
+
+from graphics.lib.color import Color
+from graphics.lib.drawing import Drawing
+from graphics.lib.matrix import Matrix, TransformationMatrix, EdgeMatrix
+
+import argparse
 import mdl
+import traceback
 
-def run(filename):
-    """
-    This function runs an mdl script
-    """
+class Runner():
 
-    p = mdl.parseFile(filename)
+    def __init__(self, width=512, height=512, color="#FF0000"):
+        """
+        Constructor for the Runner class.
 
-    if p:
-        (commands, symbols) = p
-    else:
-        print "Parsing failed."
-        return
-        
-    #for command in commands:
-        #print command
-    for symbol in symbols:
-        print symbol
+        Parameters:
+        width: int (optional), the width of the image to generate
+        height: int (optional), the height of the image to generate
+        color: string (optional), the hexadecimal representation of the color to
+            use when drawing
+        """
+        self.width = width
+        self.height = height
+        self.drawing = Drawing(width, height)
+        self.color = Color(color)
+
+    def run(self, filename):
+        """
+        Reads the given file, compiles the code found in it, and runs it.
+
+        Parameters:
+        filename: str, the name of the file to read from
+        """
+        try:
+            (commands, symbols) = mdl.parseFile(filename)
+        except:
+            print "Parsing failed."
+            print traceback.format_exc()
+        print commands
+
 
 if __name__ == "__main__":
-    run("dwrobot.mdl")
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("file", help="The file to generate an image from")
+    args = argparser.parse_args()
+
+    runner = Runner()
+    runner.run(args.file)
